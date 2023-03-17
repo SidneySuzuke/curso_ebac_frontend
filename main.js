cbd5ac0def82020b4c9b9a0bcf376c535cfd62a9
Sidney Sadao Suzuke <sidney.suzuke@dzyon.com>
2023-03-17 14:05:20 -0300

    exercicio 7

---
+++ main.js
@@ -0,0 +1,14 @@
+const form = document.getElementById('form-valida');
+const valora = document.getElementById('valor-a');
+const valorb = document.getElementById('valor-b');
+
+
+form.addEventListener('submit', function(e) {
+    e.preventDefault();
+    if (valorb.value <= valora.value) { 
+        alert (`Valor ${valorb.value} não é maior que valor ${valora.value}`)
+        } else {
+        alert (`Valor ${valorb.value} é maior que valor ${valora.value}`)
+        }
+})
+
